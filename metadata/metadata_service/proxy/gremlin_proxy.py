e8fedd2698ca8ba19a5e4def7f14a45ee435e0ce
Andrea Callarelli <andrea@datachef.co>
2022-02-15 09:22:41 +0100

    try using AiohttpTransport

--- metadata/metadata_service/proxy/gremlin_proxy.py
+++ metadata/metadata_service/proxy/gremlin_proxy.py
@@ -39,6 +39,7 @@ from gremlin_python.driver.driver_remote_connection import \
     DriverRemoteConnection
 from gremlin_python.driver.resultset import ResultSet
 # from gremlin_python.driver.tornado.transport import TornadoTransport
+from gremlin_python.driver.aiohttp.transport import AiohttpTransport
 from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.process.graph_traversal import (GraphTraversal,
                                                     GraphTraversalSource, V,
@@ -871,6 +872,7 @@ class AbstractGremlinProxy(BaseProxy):
         self.remote_connection: DriverRemoteConnection = DriverRemoteConnection(
             url=self.possibly_signed_ws_client_request_or_url(),
             # transport_factory=lambda: TornadoTransport(read_timeout=None, write_timeout=None),
+            transport_factory=lambda: AiohttpTransport(call_from_event_loop=True),
             **_properties_except(self.driver_remote_connection_options, 'url'))
 
         self._g: GraphTraversalSource = traversal().withRemote(self.remote_connection)
