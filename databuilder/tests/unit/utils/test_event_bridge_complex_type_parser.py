41abb017e5f42720578e0e26297734a443880e98
Sajjad Ramezani <sajjadoncall@gmail.com>
2022-07-15 17:05:05 +0430

    feat:add tests for struct and scalar type for event bridge parser

--- databuilder/tests/unit/utils/test_event_bridge_complex_type_parser.py
+++ databuilder/tests/unit/utils/test_event_bridge_complex_type_parser.py
@@ -14,7 +14,7 @@ from databuilder.utils.event_bridge_complex_type_parser import parse_event_bridg
 
 class TestEventBridgeComplexTypeParser(unittest.TestCase):
     def setUp(self) -> None:
-        self.column_key = 'hive://gold.test_schema/test_table/col1'  # change to eventbridge
+        self.column_key = 'eventbridge://gold.test_schema/test_table/col1'  # change to eventbridge
 
     def test_transform_no_complex_type(self) -> None:
         column = ColumnMetadata('col1', None, 'int', 0)
@@ -23,6 +23,28 @@ class TestEventBridgeComplexTypeParser(unittest.TestCase):
         scalar_type = ScalarTypeMetadata(name='col1',
                                          parent=column,
                                          type_str='int')
+
+        actual = parse_event_bridge_type(column.type, column.name, column)
+        self.assertEqual(actual, scalar_type)
+
+    def test_transform_scalar_type(self) -> None:
+        column = ColumnMetadata('col1', None, 'string', 0)
+        column.set_column_key(self.column_key)
+
+        scalar_type = ScalarTypeMetadata(name='col1',
+                                       parent=column,
+                                       type_str='string')
+
+        actual = parse_event_bridge_type(column.type, column.name, column)
+        self.assertEqual(actual, scalar_type)
+
+    def test_transform_additive_scalar_type(self) -> None:
+        column = ColumnMetadata('col1', None, 'string[date-time]', 0)
+        column.set_column_key(self.column_key)
+
+        scalar_type = ScalarTypeMetadata(name='col1',
+                                       parent=column,
+                                       type_str='string[date-time]')
 
         actual = parse_event_bridge_type(column.type, column.name, column)
         self.assertEqual(actual, scalar_type)
@@ -95,6 +117,23 @@ class TestEventBridgeComplexTypeParser(unittest.TestCase):
 
         with self.assertRaises(ParseException):
             parse_event_bridge_type(column.type, column.name, column)
+
+    def test_transform_valid_only_type_struct(self) -> None:
+        column = ColumnMetadata('col1', None, 'struct<object>', 0)
+        column.set_column_key(self.column_key)
+
+        struct_type = StructTypeMetadata(name='col1',
+                                         parent=column,
+                                         type_str='struct<object>')
+        inner_scalar_nest1 = ScalarTypeMetadata(name='value',
+                                                parent=struct_type,
+                                                type_str='object')
+
+        struct_type.struct_items = {'value': inner_scalar_nest1, }
+        inner_scalar_nest1.sort_order = 0
+
+        actual = parse_event_bridge_type(column.type, column.name, column)
+        self.assertEqual(actual, struct_type)
 
     def test_transform_invalid_struct_inner_type(self) -> None:
         column = ColumnMetadata('col1', None, 'struct<nest1:varchar(256)å,'
