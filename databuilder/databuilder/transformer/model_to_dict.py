f217db1c570dd2ea6f25fce9de8aedfc19d43daf
Sajjad Ramezani <sajjadoncall@gmail.com>
2022-07-22 18:01:53 +0430

    fix:loads json string for model to dict

--- databuilder/databuilder/transformer/model_to_dict.py
+++ databuilder/databuilder/transformer/model_to_dict.py
@@ -9,7 +9,7 @@ from pyhocon import ConfigTree
 
 from databuilder.transformer.base_transformer import Transformer
 from databuilder.models.elasticsearch_document import ElasticsearchDocument
-
+import json
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,7 +23,7 @@ class ModelToDict(Transformer):
         pass
 
     def transform(self, record: ElasticsearchDocument) -> Dict[str, Any]:
-        return record.to_json()
+        return json.loads(record.to_json())
 
     def get_scope(self) -> str:
         return 'transformer.model_to_dict'
